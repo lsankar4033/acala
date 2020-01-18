ab5cdc0788cf93073466db97d5d20b6ac2098904
lsankar4033 <lsankar4033@gmail.com>
2020-01-17 23:13:12 -0800

    start structuring tx pool

--- acala/tx_pool.py
+++ acala/tx_pool.py
@@ -1,13 +1,35 @@
 import asyncio
 
-class TxPool():
+from collections import defaultdict, namedtuple
 
+# TODO: add coin?
+Tx = namedtuple('Tx', ['from_address', 'to_address', 'value', 'data', 'nonce'])
+
+class TxPool():
     def __init__(self):
         self.lock = asyncio.Lock()
+        self.from_address_to_txes = defaultdict(list)
 
-    async def add_tx(self, tx):
+    async def add_tx(self, tx: Tx):
         async with self.lock:
-            pass
+            existing_txes = self.from_address_to_txes[tx.from_address]
+            if len(existing_txes) == 0:
+                existing_txes.append(tx)
+
+            elif tx.nonce == existing_txes[-1].nonce + 1:
+                existing_txes.append(tx)
+
+            elif any((tx.nonce == cmp_tx.nonce for cmp_tx in existing_txes)):
+                # TODO: replacement
+                pass
+
+            else:
+                # TODO: error!
+                pass
+            # if list empty, add tx
+            # elif 1 greater than last one in list, add it
+            # elif equal to any in list, replace old
+            # else
 
     async def retrieve_valid_batch(self, chain_state):
         async with self.lock:
